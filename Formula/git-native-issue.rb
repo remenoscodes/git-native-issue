6b5ac24150138b4b08c3b0a415df53faf74cd0f7
Emerson Soares <remenoscodes@gmail.com>
2026-02-08 15:00:31 -0300

    Update Homebrew formula test for v1.0.2
    Update test block to check for version 1.0.2 and remove outdated comment.

--- Formula/git-native-issue.rb
+++ Formula/git-native-issue.rb
@@ -2,7 +2,7 @@ class GitNativeIssue < Formula
   desc "Distributed issue tracking system built on Git"
   homepage "https://github.com/remenoscodes/git-native-issue"
   url "https://github.com/remenoscodes/git-native-issue/releases/download/v1.0.2/git-native-issue-v1.0.2.tar.gz"
-  sha256 "e9d4cdf6239a7175a651b8dc89bef1b133500306b5e7fe6cd78025f83082d40d"  # Update this after creating v1.0.1 release
+  sha256 "e9d4cdf6239a7175a651b8dc89bef1b133500306b5e7fe6cd78025f83082d40d"
   license "GPL-2.0-only"
   version "1.0.2"
 
@@ -21,6 +21,6 @@ class GitNativeIssue < Formula
 
   test do
     system "#{bin}/git-issue", "version"
-    assert_match "1.0.1", shell_output("#{bin}/git-issue version")
+    assert_match "1.0.2", shell_output("#{bin}/git-issue version")
   end
 end
